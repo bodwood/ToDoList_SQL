18c220b2d7240d1bb724bd9c41e0b7b0c78ffd30
brandonf55 <brandon.fenk99@gmail.com>
2022-12-19 15:52:36 -0800

    add new tests to ItemTests.cs Co-Authored By: bodwood <bodiewood@hotmail.com>

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -6,90 +6,136 @@ using System;
 
 namespace ToDoList.Tests
 {
-  [TestClass]
-  public class ItemTests : IDisposable
-  {
-    public IConfiguration Configuration { get; set; }//property to save appsettings.json configurations
-
-    public void Dispose()
+    [TestClass]
+    public class ItemTests : IDisposable
     {
-      Item.ClearAll();
+        public IConfiguration Configuration { get; set; }//property to save appsettings.json configurations
+
+        public void Dispose()
+        {
+            Item.ClearAll();
+        }
+
+        public ItemTests()
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder() //configuration object that invludes all data in appsettings.json
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            DBConfiguration.ConnectionString = Configuration["ConnectionStrings:TestConnection"]; //sets DBConfigurations.ConnectionString to "test connection" 
+        }                                                                                       //overrides DBConfiguration.ConnectionSTring that was set in DatabaseConfig.cs
+                                                                                                //ensures our tests are connected to our test database, instead of development database
+
+        [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      // Arrange
+      List<Item> newList = new List<Item> { };
+
+      // Act
+      List<Item> result = Item.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
     }
 
-     public ItemTests()
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
     {
-      IConfigurationBuilder builder = new ConfigurationBuilder() //configuration object that invludes all data in appsettings.json
-          .AddJsonFile("appsettings.json");
-      Configuration = builder.Build();  
-      DBConfiguration.ConnectionString = Configuration["ConnectionStrings:TestConnection"]; //sets DBConfigurations.ConnectionString to "test connection" 
-    }                                                                                       //overrides DBConfiguration.ConnectionSTring that was set in DatabaseConfig.cs
-                                                                                            //ensures our tests are connected to our test database, instead of development database
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      newItem1.Save();                           //saves new item
+      Item newItem2 = new Item(description02);
+      newItem2.Save();                           //saves new item
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
 
-    // [TestMethod]
-    // public void ItemConstructor_CreatesInstanceOfItem_Item()
-    // {
-    //   Item newItem = new Item("test");
-    //   Assert.AreEqual(typeof(Item), newItem.GetType());
-    // }
+      //Act
+      List<Item> result = Item.GetAll();
 
-    // [TestMethod]
-    // public void GetDescription_ReturnsDescription_String()
-    // {
-    //   //Arrange
-    //   string description = "Walk the dog.";
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
 
-    //   //Act
-    //   Item newItem = new Item(description);
-    //   string result = newItem.Description;
+    [TestMethod]
+    public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
+    {
+      // Arrange, Act
+      Item firstItem = new Item("Mow the lawn");
+      Item secondItem = new Item("Mow the lawn");
 
-    //   //Assert
-    //   Assert.AreEqual(description, result);
-    // }
+      // Assert
+      Assert.AreEqual(firstItem, secondItem);
+    }
 
-    // [TestMethod]
-    // public void SetDescription_SetDescription_String()
-    // {
-    //   //Arrange
-    //   string description = "Walk the dog.";
-    //   Item newItem = new Item(description);
+    [TestMethod]
+    public void Save_SavesToDatabase_ItemList()
+    {
+      //Arrange
+      Item testItem = new Item("Mow the lawn");
 
-    //   //Act
-    //   string updatedDescription = "Do the dishes";
-    //   newItem.Description = updatedDescription;
-    //   string result = newItem.Description;
+      //Act
+      testItem.Save();
+      List<Item> result = Item.GetAll();
+      List<Item> testList = new List<Item>{testItem};
 
-    //   //Assert
-    //   Assert.AreEqual(updatedDescription, result);
-    // }
+      //Assert
+      CollectionAssert.AreEqual(testList, result);
+    }
 
-    // [TestMethod]
-    // public void GetAll_ReturnsEmptyList_ItemList()
-    // {
-    //   // Arrange
-    //   List<Item> newList = new List<Item> { };
+        // [TestMethod]
+        // public void ItemConstructor_CreatesInstanceOfItem_Item()
+        // {
+        //   Item newItem = new Item("test");
+        //   Assert.AreEqual(typeof(Item), newItem.GetType());
+        // }
 
-    //   // Act
-    //   List<Item> result = Item.GetAll();
+        // [TestMethod]
+        // public void GetDescription_ReturnsDescription_String()
+        // {
+        //   //Arrange
+        //   string description = "Walk the dog.";
 
-    //   // Assert
-    //   CollectionAssert.AreEqual(newList, result);
-    // }
+        //   //Act
+        //   Item newItem = new Item(description);
+        //   string result = newItem.Description;
 
-    // [TestMethod]
-    // public void GetAll_ReturnsItems_ItemList()
-    // {
-    //   //Arrange
-    //   string description01 = "Walk the dog";
-    //   string description02 = "Wash the dishes";
-    //   Item newItem1 = new Item(description01);
-    //   Item newItem2 = new Item(description02);
-    //   List<Item> newList = new List<Item> { newItem1, newItem2 };
+        //   //Assert
+        //   Assert.AreEqual(description, result);
+        // }
 
-    //   //Act
-    //   List<Item> result = Item.GetAll();
+        // [TestMethod]
+        // public void SetDescription_SetDescription_String()
+        // {
+        //   //Arrange
+        //   string description = "Walk the dog.";
+        //   Item newItem = new Item(description);
 
-    //   //Assert
-    //   CollectionAssert.AreEqual(newList, result);
-    // }
-  }
+        //   //Act
+        //   string updatedDescription = "Do the dishes";
+        //   newItem.Description = updatedDescription;
+        //   string result = newItem.Description;
+
+        //   //Assert
+        //   Assert.AreEqual(updatedDescription, result);
+        // }
+
+
+        // [TestMethod]
+        // public void GetAll_ReturnsItems_ItemList()
+        // {
+        //   //Arrange
+        //   string description01 = "Walk the dog";
+        //   string description02 = "Wash the dishes";
+        //   Item newItem1 = new Item(description01);
+        //   Item newItem2 = new Item(description02);
+        //   List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+        //   //Act
+        //   List<Item> result = Item.GetAll();
+
+        //   //Assert
+        //   CollectionAssert.AreEqual(newList, result);
+        // }
+    }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -6,7 +6,7 @@ namespace ToDoList.Models
     public class Item
     {
         public string Description { get; set; }
-        public int Id { get; }
+        public int Id { get; set;}
 
 
         public Item(string description)
@@ -15,10 +15,31 @@ namespace ToDoList.Models
 
         }
 
-        public Item(string description, int id){
-          Description = description;
-          Id = id;
+        public Item(string description, int id)
+        {
+            Description = description;
+            Id = id;
         }
+
+        public override bool Equals(System.Object otherItem)  //equals is built-in to C#. Must use System.Object
+        {
+            if (!(otherItem is Item))  //checks if argument passed into parameter is an Item object
+            {
+                return false;
+            }
+            else
+            {
+                Item newItem = (Item)otherItem;  //converts argument passed in, to an Item object
+                bool idEquality = (this.Id == newItem.Id);
+                bool descriptionEquality = (this.Description == newItem.Description);
+                return (idEquality && descriptionEquality);
+            }
+        }
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();  //generates a hash code for each item
+        }
+
 
         public static List<Item> GetAll()
         {
@@ -48,7 +69,17 @@ namespace ToDoList.Models
 
         public static void ClearAll()
         {
+            MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString); //call new MySqlConnection(DBConfiguration.ConnectionString) to create connection
+            conn.Open();  //open connection 
 
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = "DELETE FROM items;";   //clears entire items table in database
+            cmd.ExecuteNonQuery();  //modifies data instead of querying
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
         }
         public static Item Find(int searchId)
         {
@@ -56,5 +87,32 @@ namespace ToDoList.Models
             return placeholderItem;
 
         }
+
+        public void Save()
+        {
+            MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+            conn.Open();  //opens connection
+
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+
+            // Begin new code
+
+            cmd.CommandText = "INSERT INTO items (description) VALUES (@ItemDescription);"; //@ItemDescription is a parameter placeholder
+            MySqlParameter param = new MySqlParameter();
+            param.ParameterName = "@ItemDescription";  //.ParameterName is a buildin method for giving parameters names for the parameter placeholder 
+            param.Value = this.Description; //adds value to the above parameter
+            cmd.Parameters.Add(param);
+            cmd.ExecuteNonQuery();  //executes nonquery command (modifies)
+            Id = (int) cmd.LastInsertedId; //sets the item's Id property equal to the value of teh id of the new row in the database
+
+            // End new code
+
+            conn.Close();   //closes connection
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
     }
 }
